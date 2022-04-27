f83a02180b2e520845163b6a41f15833e9434c35
OTDavid7 <92827464+OTDavid7@users.noreply.github.com>
2022-04-27 16:18:52 +0100

    ABLE VOICE FILES

---
+++ script.js
@@ -0,0 +1,11 @@
+// // alert ("Welcome to my Port-folio. Click here to continue");
+// // alert (2 + 2);
+
+
+// // // Button
+// // document.getElementById("btn").onclick = function () {
+// //     document.write("Do you want to learn more");
+// //     alert("index.html");
+// // }
+
+// alert("WELCOME TO ABLE VOICE");
